e17932ab0b09f79a65019eafdf0a0a06f1f4fad2
Arthuryh <ayhayakawa@hotmail.com>
2018-06-19 14:25:58 -0300

    aaaaaaaaaaaaaaaaa

--- TechManager/Program.cs
+++ TechManager/Program.cs
@@ -16,11 +16,7 @@ namespace TechManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             Application.Run(new frmAdvertenciaProf());
-=======
-            Application.Run(new frmAdmAlteraPerfil());
->>>>>>> 737be27a7c2246ee5709bd218722fb87d0b21c2e
         }
     }
 }
